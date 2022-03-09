95d55f40e5b6e1b7fa470115e35bd2f2af548e47
Gilmar <gilmar.jesus@gmail.com>
2022-03-08 22:00:41 -0300

    Criado configuração para usar o selenium.

--- setup/tests.py
+++ setup/tests.py
@@ -1,6 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 
+
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self):
-        pass
+        self.browser = webdriver.Chrome('/home/gilunix/Documentos/Projetos/Alura/Python/TDD_busca_animal/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
