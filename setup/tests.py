835b29016bc7166875c910df1b88cbd8d85dd346
Gilmar <gilmar.jesus@gmail.com>
2022-03-10 18:50:18 -0300

    Pagina de busca por animail implementada, orientado por testes.

--- setup/tests.py
+++ setup/tests.py
@@ -1,13 +1,19 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
+
+from animais.models import Animal
 
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/gilunix/Documentos/Projetos/Alura/Python/TDD_busca_animal/chromedriver')
-
+        self.animal = Animal.objects.create(
+            nome_animal = 'leão',
+            predador = 'Sim',
+            venenoso = 'Não',
+            domestico = 'Não'
+        )
     def tearDown(self):
         self.browser.quit()
 
@@ -23,13 +29,14 @@ class AnimaisTestCase(LiveServerTestCase):
         brand_element = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         self.assertEqual('Busca Animal', brand_element.text)
         # Ele vê um campo para pesquisar animais pelo nome.
-        buscar_animal_input = self.browser.find_element(By.CSS_SELECTOR,'input#buscar-animal')
-        self.assertEqual(buscar_animal_input.get_attribute('placeholder'),'Exemplo: leão, urso...')
+        buscar_animal_input = self.browser.find_element(By.CSS_SELECTOR, 'input#buscar-animal')
+        self.assertEqual(buscar_animal_input.get_attribute('placeholder'), 'Exemplo: leão, urso...')
         # Ele pesquisa por Leão e clica no botão pesquisar.
         buscar_animal_input.send_keys('leão')
-        self.browser.find_element(By.CSS_SELECTOR,'form button').click()
+        self.browser.find_element(By.CSS_SELECTOR, 'form button').click()
 
         # O site exibe 4 caracteristicas do animal pesquisado.
+        caracteristicas = self.browser.find_elements(By.CSS_SELECTOR, '.result-description')
+        self.assertGreater(len(caracteristicas), 3)
         # Ele desiste de adotar um leão.
 
-    # find_element(By.CSS_SELECTOR, 'exemplo')
